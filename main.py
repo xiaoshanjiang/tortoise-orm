50b027b3fd045edc2e6bf82e8975b4778ebb5237
xiaoshanjiang <jasonxiaoshanjiang@gmail.com>
2023-10-25 19:23:11 +1100

    manual sql

--- main.py
+++ main.py
@@ -1,68 +1,45 @@
 """
-This example demonstrates model signals usage
+This example demonstrates executing manual SQL queries
 """
-from typing import List, Optional, Type
-
-from tortoise import BaseDBAsyncClient, Tortoise, fields, run_async
+from tortoise import Tortoise, connections, fields, run_async
 from tortoise.models import Model
-from tortoise.signals import post_delete, post_save, pre_delete, pre_save
+from tortoise.transactions import in_transaction
 
 
-class Signal(Model):
+class Event(Model):
     id = fields.IntField(pk=True)
     name = fields.TextField()
-
-    class Meta:
-        table = "signal"
-
-    def __str__(self):
-        return self.name
-
-
-@pre_save(Signal)
-async def signal_pre_save(
-    sender: "Type[Signal]", instance: Signal, using_db, update_fields
-) -> None:
-    print(sender, instance, using_db, update_fields)
-
-
-@post_save(Signal)
-async def signal_post_save(
-    sender: "Type[Signal]",
-    instance: Signal,
-    created: bool,
-    using_db: "Optional[BaseDBAsyncClient]",
-    update_fields: List[str],
-) -> None:
-    print(sender, instance, using_db, created, update_fields)
-
-
-@pre_delete(Signal)
-async def signal_pre_delete(
-    sender: "Type[Signal]", instance: Signal, using_db: "Optional[BaseDBAsyncClient]"
-) -> None:
-    print(sender, instance, using_db)
-
-
-@post_delete(Signal)
-async def signal_post_delete(
-    sender: "Type[Signal]", instance: Signal, using_db: "Optional[BaseDBAsyncClient]"
-) -> None:
-    print(sender, instance, using_db)
+    timestamp = fields.DatetimeField(auto_now_add=True)
 
 
 async def run():
     await Tortoise.init(db_url="sqlite://:memory:", modules={"models": ["__main__"]})
     await Tortoise.generate_schemas()
-    # pre_save,post_save will be send
-    signal = await Signal.create(name="Signal")
-    signal.name = "Signal_Save"
 
-    # pre_save,post_save will be send
-    await signal.save(update_fields=["name"])
+    # Need to get a connection. Unless explicitly specified, the name should be 'default'
+    conn = connections.get("default")
 
-    # pre_delete,post_delete will be send
-    await signal.delete()
+    # Now we can execute queries in the normal autocommit mode
+    await conn.execute_query("INSERT INTO event (name) VALUES ('Foo')")
+
+    # You can also you parameters, but you need to use the right param strings for each dialect
+    await conn.execute_query("INSERT INTO event (name) VALUES (?)", ["Bar"])
+
+    # To do a transaction you'd need to use the in_transaction context manager
+    async with in_transaction("default") as tconn:
+        await tconn.execute_query("INSERT INTO event (name) VALUES ('Moo')")
+        # Unless an exception happens it should commit automatically
+
+    # This transaction is rolled back
+    async with in_transaction("default") as tconn:
+        await tconn.execute_query("INSERT INTO event (name) VALUES ('Sheep')")
+        # Rollback to fail transaction
+        await tconn.rollback()
+
+    # Consider using execute_query_dict to get return values as a dict
+    val = await conn.execute_query_dict("SELECT * FROM event")
+    print(val)
+    # Note that the result doesn't contain the rolled-back "Sheep" entry.
 
 
 if __name__ == "__main__":
