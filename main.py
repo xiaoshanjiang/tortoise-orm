0d0cf023cb18d9e80b5f1c93ca3900c4dbdf80f9
xiaoshanjiang <jasonxiaoshanjiang@gmail.com>
2023-10-25 17:50:18 +1100

    schema creation

--- main.py
+++ main.py
@@ -1,88 +1,30 @@
-from tortoise import Tortoise, run_async
-from tortoise.functions import Avg, Count, Sum
+"""
+This example demonstrates SQL Schema generation for each DB type supported.
+"""
+from tortoise import Tortoise, connections, fields, run_async
+from tortoise.models import Model
+from tortoise.utils import get_schema_sql
 
-from models.books import Author, Book
+from models.sports import Event, Team, Tournament
 
 
 async def run():
+    print("SQLite:\n")
     await Tortoise.init(db_url="sqlite://:memory:", modules={"models": ["__main__"]})
-    await Tortoise.generate_schemas()
+    sql = get_schema_sql(connections.get("default"), safe=False)
+    print(sql)
 
-    a1 = await Author.create(name="author1")
-    a2 = await Author.create(name="author2")
-    for i in range(10):
-        await Book.create(name=f"book{i}", author=a1, rating=i)
-    for i in range(5):
-        await Book.create(name=f"book{i}", author=a2, rating=i)
+    print("\n\nMySQL:\n")
+    await Tortoise.init(db_url="mysql://root:@127.0.0.1:3306/", modules={"models": ["__main__"]})
+    sql = get_schema_sql(connections.get("default"), safe=False)
+    print(sql)
 
-    ret = (
-        await Book.annotate(count=Count("id"))
-        .group_by("author_id")
-        .values("author_id", "count")
+    print("\n\nPostgreSQL:\n")
+    await Tortoise.init(
+        db_url="postgres://postgres:@127.0.0.1:5432/", modules={"models": ["__main__"]}
     )
-    print(ret)
-    # >>> [{'author_id': 1, 'count': 10}, {'author_id': 2, 'count': 5}]
-
-    ret = (
-        await Book.annotate(count=Count("id"))
-        .filter(count__gt=6)
-        .group_by("author_id")
-        .values("author_id", "count")
-    )
-    print(ret)
-    # >>> [{'author_id': 1, 'count': 10}]
-
-    ret = (
-        await Book.annotate(sum=Sum("rating"))
-        .group_by("author_id")
-        .values("author_id", "sum")
-    )
-    print(ret)
-    # >>> [{'author_id': 1, 'sum': 45.0}, {'author_id': 2, 'sum': 10.0}]
-
-    ret = (
-        await Book.annotate(sum=Sum("rating"))
-        .filter(sum__gt=11)
-        .group_by("author_id")
-        .values("author_id", "sum")
-    )
-    print(ret)
-    # >>> [{'author_id': 1, 'sum': 45.0}]
-
-    ret = (
-        await Book.annotate(avg=Avg("rating"))
-        .group_by("author_id")
-        .values("author_id", "avg")
-    )
-    print(ret)
-    # >>> [{'author_id': 1, 'avg': 4.5}, {'author_id': 2, 'avg': 2.0}]
-
-    ret = (
-        await Book.annotate(avg=Avg("rating"))
-        .filter(avg__gt=3)
-        .group_by("author_id")
-        .values("author_id", "avg")
-    )
-    print(ret)
-    # >>> [{'author_id': 1, 'avg': 4.5}]
-
-    # and use .values_list()
-    ret = (
-        await Book.annotate(count=Count("id"))
-        .group_by("author_id")
-        .values_list("author_id", "count")
-    )
-    print(ret)
-    # >>> [(1, 10), (2, 5)]
-
-    # group by with join
-    ret = (
-        await Book.annotate(count=Count("id"))
-        .group_by("author__name")
-        .values("author__name", "count")
-    )
-    print(ret)
-    # >>> [{"author__name": "author1", "count": 10}, {"author__name": "author2", "count": 5}]
+    sql = get_schema_sql(connections.get("default"), safe=False)
+    print(sql)
 
 
 if __name__ == "__main__":
--- models/sports.py
+++ models/sports.py
@@ -3,34 +3,39 @@ from tortoise import Model, fields
 
 class Tournament(Model):
     id = fields.IntField(pk=True)
-    name = fields.TextField()
-    desc = fields.TextField(null=True)
+    name = fields.CharField(max_length=255, description="Tournament name", index=True)
+    created = fields.DatetimeField(auto_now_add=True, description="Created datetime")
 
     events: fields.ReverseRelation["Event"]
 
-    def __str__(self):
-        return self.name
+    class Meta:
+        table_description = "What Tournaments we have"
 
 
 class Event(Model):
-    id = fields.IntField(pk=True)
-    name = fields.TextField()
+    id = fields.IntField(pk=True, description="Event ID")
+    name = fields.CharField(max_length=255, unique=True)
     tournament: fields.ForeignKeyRelation[Tournament] = fields.ForeignKeyField(
-        "models.Tournament", related_name="events"
+        "models.Tournament", related_name="events", description="FK to tournament"
     )
     participants: fields.ManyToManyRelation["Team"] = fields.ManyToManyField(
-        "models.Team", related_name="events", through="event_team"
+        "models.Team",
+        related_name="events",
+        through="event_team",
+        description="How participants relate",
     )
+    modified = fields.DatetimeField(auto_now=True)
+    prize = fields.DecimalField(max_digits=10, decimal_places=2, null=True)
+    token = fields.CharField(max_length=100, description="Unique token", unique=True)
 
-    def __str__(self):
-        return self.name
+    class Meta:
+        table_description = "This table contains a list of all the events"
 
 
 class Team(Model):
-    id = fields.IntField(pk=True)
-    name = fields.TextField()
+    name = fields.CharField(max_length=50, pk=True, description="The TEAM name (and PK)")
 
     events: fields.ManyToManyRelation[Event]
 
-    def __str__(self):
-        return self.name
+    class Meta:
+        table_description = "The TEAMS!"
