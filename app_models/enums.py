e3f4825692ed1d39d6d46eb30a53d841274cc9e6
xiaoshanjiang <jasonxiaoshanjiang@gmail.com>
2023-10-25 19:13:03 +1100

    enum fiels

---
+++ app_models/enums.py
@@ -0,0 +1,20 @@
+from enum import Enum, IntEnum
+
+from tortoise import Model, fields
+
+
+class Service(IntEnum):
+    python_programming = 1
+    database_design = 2
+    system_administration = 3
+
+
+class Currency(str, Enum):
+    HUF = "HUF"
+    EUR = "EUR"
+    USD = "USD"
+
+
+class EnumFields(Model):
+    service: Service = fields.IntEnumField(Service)
+    currency: Currency = fields.CharEnumField(Currency, default=Currency.HUF)
--- main.py
+++ main.py
@@ -1,41 +1,29 @@
-"""
-This example shows how relations between models especially unique field work.
-
-Key points in this example are use of ForeignKeyField and OneToOneField has to_field.
-For other basic parts, it is the same as relation example.
-"""
 from tortoise import Tortoise, run_async
 
-from app_models.employees import Employee
+from app_models.enums import Currency, EnumFields, Service
 
 
 async def run():
     await Tortoise.init(db_url="sqlite://:memory:", modules={"models": ["__main__"]})
     await Tortoise.generate_schemas()
 
-    root = await Employee.create(name="Root")
-    loose = await Employee.create(name="Loose")
-    _1 = await Employee.create(name="1. First H1", manager=root)
-    _2 = await Employee.create(name="2. Second H1", manager=root)
-    _1_1 = await Employee.create(name="1.1. First H2", manager=_1)
-    _1_1_1 = await Employee.create(name="1.1.1. First H3", manager=_1_1)
-    _2_1 = await Employee.create(name="2.1. Second H2", manager=_2)
-    _2_2 = await Employee.create(name="2.2. Third H2", manager=_2)
+    obj0 = await EnumFields.create(
+        service=Service.python_programming, currency=Currency.USD
+    )
+    # also you can use valid int and str value directly
+    await EnumFields.create(service=1, currency="USD")
 
-    await _1.talks_to.add(_2, _1_1_1, loose)
-    await _2_1.gets_talked_to.add(_2_2, _1_1, loose)
+    try:
+        # invalid enum value will raise ValueError
+        await EnumFields.create(service=4, currency="XXX")
+    except ValueError:
+        print("Value is invalid")
 
-    # Evaluated off creation objects
-    print(await loose.full_hierarchy__fetch_related())
-    print(await root.full_hierarchy__async_for())
-    print(await root.full_hierarchy__fetch_related())
-
-    # Evaluated off new objects → Result is identical
-    root2 = await Employee.get(name="Root")
-    loose2 = await Employee.get(name="Loose")
-    print(await loose2.full_hierarchy__fetch_related())
-    print(await root2.full_hierarchy__async_for())
-    print(await root2.full_hierarchy__fetch_related())
+    await EnumFields.filter(pk=obj0.pk).update(
+        service=Service.database_design, currency=Currency.HUF
+    )
+    # also you can use valid int and str value directly
+    await EnumFields.filter(pk=obj0.pk).update(service=2, currency="HUF")
 
 
 if __name__ == "__main__":
